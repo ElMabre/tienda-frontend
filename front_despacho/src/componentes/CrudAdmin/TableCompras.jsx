bd6f7ccdfbf0f31ad46de87e68135c4e3396de7e
Matias Ignacio Guzman <101336792+ElMabre@users.noreply.github.com>
2026-05-26 20:15:26 -0400

    Update TableCompras.jsx

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,7 +7,7 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://44.222.141.48:8082/api/v1/ventas", {
+    await axios.get("http://13.220.9.127:8082/api/v1/ventas", {
       headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json'
